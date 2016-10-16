aaf121d855174b3524258dd8c61199a3c3b2c62f
winio94 <winio94@gmail.com>
2016-10-16 12:25:58 +0200

    add Header controller

--- src/js/controller/HeaderCtrl.js
+++ src/js/controller/HeaderCtrl.js
@@ -1,3 +1,47 @@
-app.controller("HeaderCtrl", function($scope, $http) {
-  $scope.message = "HEADER";
+app.controller("HeaderCtrl",  function($rootScope, $scope, $http, $location) {
+
+  var authenticate = function(credentials, callback) {
+    $rootScope.headers = credentials ? {authorization : "Basic " + btoa(credentials.username + ":" + credentials.password)} : {};
+    $http.get('http://localhost:8080/user', {headers : $rootScope.headers})
+    .success(function(data) {
+      if (data.name) {
+        $rootScope.authenticated = true;
+      } else {
+        $rootScope.authenticated = false;
+      }
+      callback && callback();
+    }).error(function() {
+      $rootScope.authenticated = false;
+      callback && callback();
+    });
+  };
+
+  if($rootScope.authenticated === false) {
+    authenticate();
+  }
+
+  if(!$rootScope.credentials) {
+    $rootScope.credentials = {};
+  }
+
+  $scope.login = function() {
+    authenticate($rootScope.credentials, function() {
+      if ($rootScope.authenticated) {
+        $location.path("/");
+        $scope.error = false;
+      } else {
+        $location.path("/login");
+        $scope.error = true;
+      }
+    });
+  };
+
+  $scope.logout = function() {
+    $http.post('logout', {}).success(function() {
+      $rootScope.authenticated = false;
+      $location.path("/");
+    }).error(function(data) {
+      $rootScope.authenticated = false;
+    });
+  };
 });
