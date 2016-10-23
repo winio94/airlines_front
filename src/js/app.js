7db98a4fd5cee9fcc9ed714b7c01128b3af922f0
winio94 <winio94@gmail.com>
2016-10-23 21:38:38 +0200

    add searching source and destination at flight page

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,4 @@
-var app = angular.module("mainModule", ['ngRoute']);
+var app = angular.module("mainModule", ['ngRoute','ui.select', 'ngSanitize']);
 
 app.factory('airlinesRequestInterceptor', ['$q', '$location', '$injector', function($q, $location,$injector){
 	return {
--- src/js/controller/flightCtrl.js
+++ src/js/controller/flightCtrl.js
@@ -1,6 +1,15 @@
-app.controller("FlightCtrl", function($scope, $http, PathService){
-  $http.get(PathService.getPath() + 'flights')
-    .then(function(response) {
-        $scope.flights = response.data._embedded.flights;
+app.controller("FlightCtrl", ['$scope', 'FlightService', 'AirportService', function($scope,FlightService, AirportService){
+  $scope.getFlights = function() {
+    FlightService.getFlights().then(function(response) {
+      $scope.flights = response;
     });
-});
+  };
+
+  $scope.getFlights();
+
+  $scope.getAirportsByCityName = function(cityName)  {
+    AirportService.findAirportsByCityName(cityName).then(function(response) {
+      $scope.airports = response;
+    })
+  };
+}]);
