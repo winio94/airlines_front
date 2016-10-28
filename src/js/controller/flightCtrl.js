bfb8ae1983b21831949b1362d6746c9fe729049d
winio94 <winio94@gmail.com>
2016-10-28 13:24:38 +0200

    add last part of flight page

--- src/js/controller/flightCtrl.js
+++ src/js/controller/flightCtrl.js
@@ -1,11 +1,21 @@
 app.controller("FlightCtrl", ['$scope', 'FlightService', 'AirportService', function($scope,FlightService, AirportService){
+
+  //-------------------------------VARIABLES-------------------------------//
+  $scope.flightClasses = ["Economy", "Premium Economy", "Business class", "First class"];
+  $scope.isClassEnabled = false;
+  $scope.selectedFlight = {
+    from : null,
+    to: null,
+    flightClass: null,
+    passengers: 1
+  };
+
+  //-------------------------------FUNCTIONS-------------------------------//
   $scope.getFlights = function() {
     FlightService.getFlights().then(function(response) {
       $scope.flights = response;
     });
   };
-
-  $scope.getFlights();
 
   $scope.getAirportsByCityName = function(cityName)  {
     AirportService.findAirportsByCityName(cityName).then(function(response) {
@@ -13,14 +23,15 @@ app.controller("FlightCtrl", ['$scope', 'FlightService', 'AirportService', funct
     })
   };
 
-  $scope.selectedAirport = {
-    from : null,
-    to: null
-  };
-  
   $scope.swapFlightLocations = function() {
-    var temp = $scope.selectedAirport.from;
-    $scope.selectedAirport.from = $scope.selectedAirport.to;
-    $scope.selectedAirport.to = temp;
+    var temp = $scope.selectedFlight.from;
+    $scope.selectedFlight.from = $scope.selectedFlight.to;
+    $scope.selectedFlight.to = temp;
   };
+
+  $scope.showClassOption = function() {
+    $scope.isClassEnabled = !$scope.isClassEnabled;
+  };
+
+  $scope.getFlights();
 }]);
