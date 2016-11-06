10044e207bba8b40cbbdea641598ef9438f6ced5
winio94 <winio94@gmail.com>
2016-11-06 15:05:34 +0100

    create directive for three check boxes

---
+++ src/js/directives/tripleCheckBox.js
@@ -0,0 +1,20 @@
+app.directive('ngTripleCheckBox', function() {
+  return {
+    restrict: 'E',
+    scope: {
+      'heading': '@?',
+      'leftInputHeading': '@?',
+      'rightInputHeading': '@?',
+      'centerInputHeading': '@?',
+      'leftDescription' : '@?',
+      'rightDescription' : '@?',
+      'centerDescription' : '@?',
+      'leftLabel' : '=',
+      'centerLabel' : '=',
+      'rightLabel' : '=',
+      'labelComment' : '@?',
+      'model' : '='
+    },
+    templateUrl: '/src/html/tripleCheckBox.html'
+  }
+});
