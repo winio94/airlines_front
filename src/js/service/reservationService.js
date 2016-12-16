17f61d24d18cae677daf28578741ba5b5a9d3490
winio94 <winio94@gmail.com>
2016-12-16 21:22:49 +0100

    add customer property for reservation body

--- src/js/service/reservationService.js
+++ src/js/service/reservationService.js
@@ -1,41 +1,49 @@
-app.service('ReservationService',['$http', 'PathService', '$location', '$localStorage', function($http, PathService, $location, $localStorage) {
+app.service('ReservationService',['$http', '$q', 'PathService', '$location', '$localStorage', function($http, $q, PathService, $location, $localStorage) {
   var o = {
   };
 
   o.makeReservation = function(reservation) {
-    return $http({
-      method: 'POST',
-      url: PathService.getPath() + 'reservations',
-      data : {
-        "price" : reservation.price,
-        "flight" : reservation.flight._links.self.href,
-        "passengers" : reservation.passengers,
-        "contact" : {
-          "email" : reservation.contact.email,
-          "phone" : reservation.contact.phone
-        },
-        "reservationDate" : getNowInMiliseconds()
-      }
-    }).then(
-      function(response) {
-        $localStorage.reservation = response.data;
-        $localStorage.reservation.flightInfo = reservation.flightInfo;
-        $location.path('reservation_summary')
-      }, function(data) {
-        $localStorage.reservationError = data;
+    return findCustomer().then(function(response) {
+      return $http({
+        method: 'POST',
+        url: PathService.getPath() + 'reservations',
+        data : {
+          "price" : reservation.price,
+          "flight" : reservation.flight._links.self.href,
+          "passengers" : reservation.passengers,
+          "contact" : {
+            "email" : reservation.contact.email,
+            "phone" : reservation.contact.phone
+          },
+          "reservationDate" : (new Date).format('YYYY-MM-DD HH:mm'),
+          "customer" : response !== null ? response.data._links.self.href : null
+        }
+      }).then(
+        function(response) {
+          $localStorage.reservation = response.data;
+          $localStorage.reservation.flightInfo = reservation.flightInfo;
+          $location.path('reservation_summary')
+        }, function(data) {
+          $localStorage.reservationError = data;
+        });
       });
     };
 
-    o.createTicket = function(reservation) {
-      return $http({
-        method: 'POST',
-        url: PathService.getPath() + 'tickets',
-        data:reservation.reservationCode
-      }).then(function(response){
-        $location.path('flights')
-      }, function(data) {
-        $location.path('/')
-      });
+    // TODO: implement sending tickets
+    // o.createTicket = function(reservation) {
+    //   return $http({
+    //     method: 'POST',
+    //     url: PathService.getPath() + 'tickets',
+    //     data:reservation.reservationCode
+    //   }).then(function(response){
+    //     $location.path('flights')
+    //   }, function(data) {
+    //     $location.path('/')
+    //   })
+    // };
+
+    o.confirmReservation = function(reservation) {
+      $location.path('flights');
     };
 
     o.getReservation = function() {
@@ -46,8 +54,12 @@ app.service('ReservationService',['$http', 'PathService', '$location', '$localSt
       return $localStorage.reservationError;
     };
 
-    function getNowInMiliseconds() {
-      return new Date().getTime();
+    function findCustomer() {
+      var principal = $localStorage.principal;
+      if(principal && principal.id) {
+        return $http.get(PathService.getPath() + 'customers/search/findCustomerByUserId?id=' + principal.id);
+      }
+      return $q.when(null);
     }
 
     return o;
