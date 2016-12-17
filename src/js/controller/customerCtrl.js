6fa8d3294e19ea439387e9f5a9e318abf3b03b61
winio94 <winio94@gmail.com>
2016-12-17 15:04:03 +0100

    add method for showing details

--- src/js/controller/customerCtrl.js
+++ src/js/controller/customerCtrl.js
@@ -3,14 +3,19 @@ app.controller('CustomerCtrl', ['customer', '$scope', '$http', function(customer
   $scope.activeReservations = $scope.customer.reservations.filter(onlyActiveFilter);
   $scope.finalizedReservations = $scope.customer.reservations.filter(notActiveFilter);
 
-  console.log(customer);
+  $scope.showDetails = function () {
+    $(function () {
+      $('#some-button').on('click', function () {
+        $('#overlay, #overlay-back').fadeIn(1000);
+      });
+    });
+  };
+
   function onlyActiveFilter(reservation) {
-    return true;
-    // return reservation.active === true;
+    return reservation.active === true;
   };
 
   function notActiveFilter(reservation) {
-    return true;
-    // return reservation.active === false;
+    return reservation.active === false;
   };
 }]);
