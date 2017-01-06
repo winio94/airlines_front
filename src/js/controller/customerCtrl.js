5d82ac1645ac34cc3db778513184a379479d9665
winio94 <winio94@gmail.com>
2017-01-06 12:42:09 +0100

    add account delete support

--- src/js/controller/customerCtrl.js
+++ src/js/controller/customerCtrl.js
@@ -1,59 +1,78 @@
-app.controller('CustomerCtrl', ['customer', '$scope', '$http', 'ReservationService', '$route', '$routeParams', 'PathService', 'AuthService', '$location',
-function(customer, $scope, $http, ReservationService, $route, $routeParams, PathService, AuthService, $location) {
-  $scope.customer = customer;
-  $scope.activeReservations = $scope.customer.reservations.filter(onlyActiveFilter);
-  $scope.finalizedReservations = $scope.customer.reservations.filter(notActiveFilter);
+//noinspection JSUnresolvedFunction
+app.controller('CustomerCtrl', ['customerData', '$scope', '$http', 'ReservationService', '$route', '$routeParams', 'PathService', 'AuthService', '$location',
+  function (customerData, $scope, $http, ReservationService, $route, $routeParams, PathService, AuthService, $location) {
+    $scope.customer = customerData.customer;
+    $scope.allReservations = customerData.allReservations._embedded.reservations;
+    $scope.activeReservations = $scope.allReservations.filter(onlyActiveFilter);
+    $scope.finalizedReservations = $scope.allReservations.filter(notActiveFilter);
 
-  $scope.showDetails = function () {
-    $(function () {
-      $('#some-button').on('click', function () {
-        $('#overlay, #overlay-back').fadeIn(1000);
+    $scope.showDetails = function () {
+      $(function () {
+        $('#some-button').on('click', function () {
+          $('#overlay, #overlay-back').fadeIn(1000);
+        });
       });
-    });
-  };
+    };
 
-  $scope.removeReservation = function(reservation, reservationCode) {
-    ReservationService.removeReservation(reservation, reservationCode)
-    .then(function(data) {
-      if(badRequest(data)) {
+    $scope.removeReservation = function (reservation, reservationCode) {
+      console.log(reservation);
+      ReservationService.removeReservation(reservation, reservationCode)
+        .then(function (data) {
+          if (badRequest(data)) {
+            $scope.error = true;
+          } else {
+            $scope.error = false;
+            $route.reload();
+          }
+        });
+    };
+
+    $scope.updatePassword = function (account) {
+      $http({
+        method: "PUT",
+        url: PathService.getPath() + 'customers/' + $scope.principal.id,
+        data: {
+          "oldPassword": account.oldPassword,
+          "newPassword": account.newPassword,
+          "newPasswordRetype": account.newPasswordRetype
+        }
+      }).then(function (data) {
+        if (badRequest(data)) {
+          $scope.error = true;
+        } else {
+          $scope.error = false;
+          AuthService.logout();
+        }
+      }, function (response) {
         $scope.error = true;
-      } else {
-        $scope.error = false;
-        $route.reload();
-      }
-    });
-  };
+      })
+    };
 
-  $scope.updatePassword = function(account) {
-    $http({
-      method: "PUT",
-      url: PathService.getPath() + 'customers/' + $scope.principal.id,
-      data: {
-        "oldPassword" : account.oldPassword,
-        "newPassword" : account.newPassword,
-        "newPasswordRetype" : account.newPasswordRetype
-      }
-    }).then(function(data) {
-      if(badRequest(data)) {
+    $scope.removeAccount = function () {
+      $http({
+        method: "DELETE",
+        url: PathService.getPath() + 'customers/' + $scope.principal.id
+      }).then(function (data) {
+        if (badRequest(data)) {
+          $scope.error = true;
+        } else {
+          $scope.error = false;
+          AuthService.logout();
+        }
+      }, function (response) {
         $scope.error = true;
-      } else {
-        $scope.error = false;
-        AuthService.logout();
-      }
-    }, function(response) {
-      $scope.error = true;
-    })
-  };
+      })
+    };
 
-  function badRequest(data) {
-    return data && data.status === 400;
-  };
+    function badRequest(data) {
+      return data && data.status === 400;
+    };
 
-  function onlyActiveFilter(reservation) {
-    return reservation.active === true;
-  };
+    function onlyActiveFilter(reservation) {
+      return reservation.active === true;
+    };
 
-  function notActiveFilter(reservation) {
-    return reservation.active === false;
-  };
-}]);
+    function notActiveFilter(reservation) {
+      return reservation.active === false;
+    };
+  }]);
